bc50cdd1e83eea1921f06a238e827104a2a963fc
VarunParab <90065369+VarunParab@users.noreply.github.com>
2024-12-16 11:28:57 +0530

    replaced local host

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as hoistNonReactStatics from "hoist-non-react-statics";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Alias for cleaner imports
+    },
+  },
   server: {
-    // port: 8001,
+    host: "digitalvasai.com", // Set host to your custom domain
+    port: 80, // Default HTTP port
+  },
+  build: {
+    rollupOptions: {
+      input: "index.html", // Explicitly define input
+    },
   },
   optimizeDeps: {
-    include: ["hoist-non-react-statics"],
+    include: ["hoist-non-react-statics"], // Pre-bundle dependencies
   },
 });
